8389e9dde52a01473c069a304999558bfa421ede
Jurian Baas <jurian.baas@gmail.com>
2020-04-22 22:49:57 +0200

    show d3 bar chart in table

---
+++ src/components/historyChart.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="container" class="svg-container" align="center">
+    <!-- <h1>{{ title }}</h1> -->
+    <svg :width="svgWidth" :height="svgHeight">
+      <g>
+        <rect
+          v-for="(item, index) in dayData"
+          class="bar-positive"
+          :key="`bar-${index}`"
+          :x="xScale(index)"
+          :y="yScale(item)"
+          rx="3"
+          :width="xScale.bandwidth()"
+          :height="
+            svgHeight - yScale(item) - 20 > 0
+              ? svgHeight - yScale(item) - 15
+              : 0
+          "
+        ></rect>
+        <text
+          v-for="(item, index) in dayData"
+          :key="`text-${index}`"
+          :x="xScale(index) + xScale.bandwidth() / 2"
+          :y="svgHeight"
+        >
+          <tspan v-if="ticks.includes(index)">{{ index }}</tspan>
+        </text>
+        <rect
+          class="bar-current"
+          rx="3"
+          :x="xScale(currentMeasurement.hour)"
+          :y="yScale(currentMeasurement.measurement)"
+          :width="xScale.bandwidth()"
+          :height="
+            svgHeight - yScale(currentMeasurement.measurement) - 15 > 0
+              ? svgHeight - yScale(currentMeasurement.measurement) - 15
+              : 0
+          "
+        ></rect>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+import { scaleLinear, scaleBand } from 'd3-scale';
+import { max, min } from 'd3-array';
+import { selectAll } from 'd3-selection';
+import { transition } from 'd3-transition';
+
+export default {
+  name: 'BarChart',
+  props: {
+    title: String,
+    dayData: Array,
+    currentMeasurement: Object
+  },
+  mounted() {
+    this.svgWidth = document.getElementById('container').offsetWidth * 0.85;
+    this.AddResizeListener();
+    // this.AnimateLoad();
+  },
+  data: () => ({
+    svgWidth: 0,
+    redrawToggle: true,
+    ticks: [3, 6, 9, 12, 15, 18, 21]
+  }),
+  methods: {
+    AddResizeListener() {
+      window.addEventListener('resize', () => {
+        this.$data.svgWidth =
+          document.getElementById('container').offsetWidth * 0.85;
+      });
+    }
+  },
+  computed: {
+    dataMax() {
+      return max(this.dayData, d => {
+        return d;
+      });
+    },
+    dataMin() {
+      return min(this.dayData, d => {
+        return d;
+      });
+    },
+    xScale() {
+      return scaleBand()
+        .rangeRound([0, this.svgWidth])
+        .padding(0.1)
+        .domain(this.dayData.map((d, index) => index));
+    },
+    yScale() {
+      return scaleLinear()
+        .rangeRound([this.svgHeight - 25, 0])
+        .domain([this.dataMin > 0 ? 0 : this.dataMin, this.dataMax]);
+    },
+    svgHeight() {
+      // return this.svgWidth / 1.61803398875; // golden ratio
+      return this.svgWidth / 4; // golden ratio
+    }
+  }
+};
+</script>
+
+<style scoped>
+.bar-positive {
+  fill: steelblue;
+  transition: r 0.2s ease-in-out;
+  opacity: 0.8;
+  border-radius: 3px 3px 0 0;
+}
+.bar-current {
+  fill: hotpink;
+  opacity: 0.5;
+  transition: r 0.2s ease-in-out;
+}
+
+.bar-positive:hover {
+  fill: hotpink;
+}
+
+.svg-container {
+  display: inline-block;
+  position: relative;
+  width: 100%;
+  padding-bottom: 1%;
+  vertical-align: top;
+  overflow: hidden;
+}
+
+text {
+  text-anchor: middle;
+  font-size: 0.675rem;
+}
+</style>
