c4244c22dbe5d4dcaeb1a9d5d161025216860dbe
Jurian Baas <jurian.baas@gmail.com>
2020-05-13 16:43:13 +0200

    don't prefetch home chunk 🙈

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,10 @@
 var webpack = require('webpack');
 
-// module.exports = {
-//   configureWebpack: {
-//     plugins: [
-//       new webpack.ProvidePlugin({
-//         mapboxgl: "mapbox-gl"
-//       })
-//     ]
-//   }
-// };
 
 module.exports = {
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch');
+},
   configureWebpack: {
     devServer: {
       host: 'localhost',
