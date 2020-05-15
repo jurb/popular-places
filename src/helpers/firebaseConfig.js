03996c3c7402102d0f76879f6523250c6bf95406
Jurian Baas <jurian.baas@gmail.com>
2020-05-15 16:46:29 +0200

    Sharing firebase API is safe, but let's just use env variables for everything for consistency

--- src/helpers/firebaseConfig.js
+++ src/helpers/firebaseConfig.js
@@ -1,9 +1,9 @@
 export const config = {
-  apiKey: "AIzaSyCAvTu0thy902mcZxNLNCBWm0WZ1JD46-M",
-  authDomain: "popular-places-bc133.firebaseapp.com",
-  databaseURL: "https://popular-places-bc133.firebaseio.com",
-  projectId: "popular-places-bc133",
-  storageBucket: "popular-places-bc133.appspot.com",
-  messagingSenderId: "907231229309",
-  appId: "1:907231229309:web:67919743e91699b2f8c814"
+  apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_FIREBASE_APPID
 };
